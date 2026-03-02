03f881f5019114ddbe81a5dd89b1845988224e7e
venkateshbizzhubdev <venkateshbizzhubdev@gmail.com>
2026-03-02 11:01:39 +0530

    Remove Content component and revert to using MotionHooksExample in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import Content from "@/components/content";
-
-export default function Home() {
-  return <Content />;
-=======
 import { MotionHooksExample } from "@/components/motion-hook-example";
 
 export default function Home() {
@@ -12,5 +6,4 @@ export default function Home() {
       <MotionHooksExample />
     </div>
   );
->>>>>>> b8edd45fc8b922bdc69bddede62181832f92faa1
 }
